48dab282ab9eaf377fd34f954fff1eda62af430d
Lendherm <122584856+Lendherm@users.noreply.github.com>
2025-10-06 15:19:47 -0600

    Moved index.html into src
    We moved index.html into src as well as the public and admin folders

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "./", // rutas relativas, ideal para Netlify y GitHub Pages
+  base: "./", // rutas relativas, ideal para Netlify o GitHub Pages
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        admin: resolve(__dirname, "admin/index.html"),
+        main: resolve(__dirname, "src/index.html"),
+        admin: resolve(__dirname, "src/admin/index.html"),
       },
     },
   },
+  publicDir: resolve(__dirname, "src/public"), // ⚡ para que Vite use src/public como carpeta pública
 });
