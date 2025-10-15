18e1f9b8b9872eaa6853598733f7a84abcd3d241
Luis Emilio Nathanael De la rosa Meneses <dluisemilionathanael@gmail.com>
2025-10-14 20:19:15 -0600

    Update credentials.js

--- src/PortalVisaHelper/scripts/credentials.js
+++ src/PortalVisaHelper/scripts/credentials.js
@@ -1,5 +1,5 @@
 // credentials.js
 export const CREDENTIALS = {
-    GOOGLE_CLIENT_ID: "186338226281-1r6htb4pmhp7bhcq5ihje3nuslbqhddn.apps.googleusercontent.com",
+    GOOGLE_CLIENT_ID: "",
     GOOGLE_API_KEY: "TU_API_KEY_AQUI"
 };
